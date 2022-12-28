5338682210005be717687f391caf488a64cc1aa5
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2022-12-28 15:44:06 +0530

    Add files via upload

---
+++ components/title.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Title = () => {
+
+    return(
+      <View>
+          <Text> Quizller</Text>
+      </View>
+
+    )
+} 
+ 
+export default Title
+
+const styles = StyleSheet.create({
+ 
+}); 
